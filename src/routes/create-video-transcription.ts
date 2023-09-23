57deb6db43b0d276196130d67e6275eddd19f132
joao.souza <joao.souza@vetta.digital>
2023-09-23 10:55:12 -0300

    feat: Finished completion route and setup cors

--- src/routes/create-video-transcription.ts
+++ src/routes/create-video-transcription.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { createReadStream } from "fs";
 import z from "zod";
+import { openai } from "../lib/openai";
 import { prisma } from "../lib/prisma";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
@@ -26,9 +27,26 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
 
     const audioReadStream = createReadStream(videoPath);
 
-    return {
-      videoId,
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: "whisper-1",
+      language: "pt",
+      response_format: "json",
+      temperature: 0,
       prompt,
-    };
+    });
+
+    const transcription = response.text;
+
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription,
+      },
+    });
+
+    return { transcription };
   });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
+import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
 import { createTranscriptionRoute } from "./routes/create-video-transcription";
+import { generateAiCompletionsRoute } from "./routes/generate-ai-completion";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { getAllVideosRoute } from "./routes/get-all-videos";
 import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify();
 
+app.register(fastifyCors, {
+  origin: "*",
+});
+//O ideal é que em produção seja colocado exatamente o endereço do frontend que irá acessar a aplicação
+
 app.register(getAllPromptsRoute);
 app.register(getAllVideosRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateAiCompletionsRoute);
 
 app
   .listen({
