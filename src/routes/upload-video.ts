5e4fc35c63fd3a7f7775ac9be60839d19bcd1ffc
joao.souza <joao.souza@vetta.digital>
2023-09-23 09:49:51 -0300

    feat: add new routes and start integrating with openai

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -1,39 +1,56 @@
-import { FastifyInstance } from "fastify";
-import { prisma } from "../lib/prisma";
-import { fastifyMultipart } from '@fastify/multipart'
-import path from "path";
-import fs from "fs";
-import { promisify } from 'util'
-import { pipeline } from 'stream'
-import { randomUUID } from "crypto";
+import { fastifyMultipart } from "@fastify/multipart"
+import { randomUUID } from "crypto"
+import { FastifyInstance } from "fastify"
+import fs from "fs"
+import path from "path"
+import { pipeline } from "stream"
+import { promisify } from "util"
+import { prisma } from "../lib/prisma"
 
 const pump = promisify(pipeline)
 
 export async function uploadVideoRoute(app: FastifyInstance) {
-    app.register(fastifyMultipart, {
-        limits: {
-            fileSize: 1_048_576 * 25,
-        }
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1_048_576 * 25,
+    },
+  })
+
+  app.post("/videos", async (request, reply) => {
+    const data = await request.file()
+
+    if (!data) {
+      return reply.status(400).send({ error: "Missing file" })
+    }
+
+    const extension = path.extname(data.filename)
+
+    if (extension != ".mp3") {
+      return reply
+        .status(400)
+        .send({ error: "Invalid file type, please upload a .mp3 file" })
+    }
+
+    const fileBaseName = path.basename(data.filename, extension)
+    const fileUploadName = `${fileBaseName}-${randomUUID()}-${extension}`
+
+    const uploadDestination = path.resolve(
+      __dirname,
+      "../../tmp",
+      fileUploadName
+    )
+
+    await pump(data.file, fs.createWriteStream(uploadDestination))
+
+    const video = await prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadDestination,
+      },
     })
 
-    app.post('/videos', async (request, reply) => {
-        const data = await request.file()
-
-        if(!data) {
-            return reply.status(400).send({error: 'Missing file'})
-        }
-
-        const extension = path.extname(data.filename)
-
-        if(extension !== '.mp3'){
-            return reply.status(400).send({error: 'Invalid file type, please upload a .mp3 file'}) 
-        }
-
-        const fileBaseName = path.basename(data.filename, extension)
-        const fileUploadName = `${fileBaseName}-${randomUUID()}-${extension}`
-
-        const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
-        
-        await pump(data.file, fs.createWriteStream(uploadDestination))
-    })
+    return {
+      video,
+    }
+  })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
-import { fastify } from 'fastify'
-import { prisma } from './lib/prisma'
-import { getAllPromptsRoute } from './routes/get-all-prompts'
-import { getAllVideosRoute } from './routes/get-all-videos'
-import { uploadVideoRoute } from './routes/upload-video'
+import { fastify } from "fastify";
+import { createTranscriptionRoute } from "./routes/create-video-transcription";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { getAllVideosRoute } from "./routes/get-all-videos";
+import { uploadVideoRoute } from "./routes/upload-video";
 
-const app = fastify()
+const app = fastify();
 
-app.register(getAllPromptsRoute)
-app.register(getAllVideosRoute)
-app.register(uploadVideoRoute)
+app.register(getAllPromptsRoute);
+app.register(getAllVideosRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
 
-
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() => {
-    console.log('Server is running 🚀')
-})
+  })
+  .then(() => {
+    console.log("Server is running 🚀");
+  });
