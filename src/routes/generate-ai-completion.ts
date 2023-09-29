89d0fbb98fcde0a6dcda1e1d408906c3fa450348
joao.souza <joao.souza@vetta.digital>
2023-09-28 23:20:39 -0300

    feat: Improved routes and implemented a seed to the database

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,3 +1,4 @@
+import { OpenAIStream, streamToResponse } from "ai";
 import { FastifyInstance } from "fastify";
 import z from "zod";
 import { openai } from "../lib/openai";
@@ -7,11 +8,11 @@ export async function generateAiCompletionsRoute(app: FastifyInstance) {
   app.post("/ai/complete", async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     });
 
-    const { videoId, template, temperature } = bodySchema.parse(req.body);
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body);
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -25,7 +26,7 @@ export async function generateAiCompletionsRoute(app: FastifyInstance) {
         .send({ error: "Video transcription was not generated yet." });
     }
 
-    const promptMessage = template.replace(
+    const promptMessage = prompt.replace(
       "'''{transcription}'''",
       video.transcription
     );
@@ -39,8 +40,15 @@ export async function generateAiCompletionsRoute(app: FastifyInstance) {
           content: promptMessage,
         },
       ],
+      stream: true,
     });
 
-    return response;
+    const stream = OpenAIStream(response);
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      },
+    });
   });
 }
